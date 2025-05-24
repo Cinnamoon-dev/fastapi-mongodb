074741ef78912cbf2f2fbcacacf55c459ec3c534
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-24 19:51:47 -0300

    feat: user add schema createad

--- app/schemas/userSchema.py
+++ app/schemas/userSchema.py
@@ -1,7 +1,15 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
+
+class UserAddSchema(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    user_type_id: str
 
 class UserEditSchema(BaseModel):
     name: Optional[str] = None
-    age: Optional[int] = None
-    course: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    user_type_id: Optional[str] = None
