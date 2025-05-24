d6ab1dca0780778563a70ba4aa39cda93aecb9e5
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-24 20:23:00 -0300

    chore: user type router included

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from app.database import test_mongo_conn
-from app.controllers import userController
+from app.controllers import userController, userTypeController
 
 
 app = FastAPI()
 
 app.include_router(userController.router)
+app.include_router(userTypeController.router)
 
 if __name__ == '__main__':
     test_mongo_conn()
