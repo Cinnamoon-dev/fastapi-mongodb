94fdb72f8d6af881b865cd10707f79b9e5abd1b8
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-24 19:35:20 -0300

    refactor: extracting serializers into another file

--- app/models/userModel.py
+++ app/models/userModel.py
@@ -4,14 +4,3 @@ class User(BaseModel):
     name: str
     age: int
     course: str
-
-def user_serial(user: User):
-    return {
-        "id": str(user["_id"]),
-        "name": user["name"],
-        "age": user["age"],
-        "course": user["course"]
-    }
-
-def list_users(users):
-    return [user_serial(user) for user in users]
