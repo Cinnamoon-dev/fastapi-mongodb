fec0f54deb07d479e3223ae4aa3b390b43e3b941
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-24 20:21:46 -0300

    feat: user type id added to user model

--- app/models/userModel.py
+++ app/models/userModel.py
@@ -1,8 +1,15 @@
 from bson import ObjectId
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, model_validator
 
 class User(BaseModel):
     name: str
     email: EmailStr
     password: str
-    user_type_id: ObjectId
+    user_type_id: str
+
+    @model_validator(mode="after")
+    def validate_user_type_id(self):
+        if not ObjectId.is_valid(self.user_type_id):
+            raise ValueError("Invalid user_type_id!")
+
+        return self
