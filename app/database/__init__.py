f69add253e67a73382492933f3069652722f4849
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-24 19:41:50 -0300

    refactor: environment variable extracted in a constant

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,13 +1,15 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.getenv("MONGO_CLIENT", "mongodb://localhost:27017"))
+MONGO_CLIENT = os.getenv("MONGO_CLIENT", "mongodb://localhost:27017")
+
+client = MongoClient(MONGO_CLIENT)
 db = client.college
 users_collection = db["users"]
 
 def test_mongo_conn():
     try:
         client.admin.command('ping')
-        print("ping db")
+        print(f"ping {MONGO_CLIENT}")
     except Exception as e:
         print(e)
