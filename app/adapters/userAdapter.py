60ad27ed01a34c7db52854bd497fda4345c9f312
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-24 19:52:29 -0300

    refactor: new user object used in user adapter

--- app/adapters/userAdapter.py
+++ app/adapters/userAdapter.py
@@ -1,4 +1,8 @@
+from bson import ObjectId
+from fastapi import HTTPException
+
 from app.models.userModel import User
+from app.schemas.userSchema import UserAddSchema
 from app.services.userService import UserService
 
 class UserAdapter:
@@ -10,9 +14,19 @@ class UserAdapter:
         user = UserService().view_one_user(id)
         return {"error": False, "data": user}
     
-    def user_add_controller(self, user: User):
-        inserterd_id = UserService().add_user(user)
-        return {"error": False, "message": f"user {inserterd_id} added successfully"}
+    def user_add_controller(self, user: UserAddSchema):
+        try:
+            new_user = User(
+                name = user.name,
+                email = user.email,
+                password = user.password,
+                user_type_id = ObjectId(user.user_type_id)
+            )
+        except Exception:
+            raise HTTPException(status_code=422, detail="User body invalid!")
+
+        inserted_id = UserService().add_user(new_user)
+        return {"error": False, "message": f"user {inserted_id} added successfully"}
     
     def user_edit_controller(self, id, fields):
         edited_id = UserService().edit_user(id, fields)
