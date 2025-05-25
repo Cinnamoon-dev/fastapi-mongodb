9b2fb7aa08d95dcf6ccfdb51dc2cfbc187dd4e85
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-25 20:20:05 -0300

    chore: awating mongo conn test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uvicorn
+import uvicorn, asyncio
 from fastapi import FastAPI
 from app.database import test_mongo_conn
 from app.controllers import userController, userTypeController
@@ -9,6 +9,7 @@ app = FastAPI()
 app.include_router(userController.router)
 app.include_router(userTypeController.router)
 
+
 if __name__ == '__main__':
-    test_mongo_conn()
+    asyncio.run(test_mongo_conn())
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
