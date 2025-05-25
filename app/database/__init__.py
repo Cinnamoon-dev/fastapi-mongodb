f8e57f8231351675166b24960b46d4f3e884ea81
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-05-25 20:19:28 -0300

    refactor: mongo client changed to async

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,16 +1,16 @@
 import os
-from pymongo import MongoClient
+from pymongo import AsyncMongoClient
 
 MONGO_CLIENT = os.getenv("MONGO_CLIENT", "mongodb://localhost:27017")
 
-client = MongoClient(MONGO_CLIENT)
+client = AsyncMongoClient(MONGO_CLIENT)
 db = client.college
 users_collection = db["users"]
 user_types_collection = db["user_types"]
 
-def test_mongo_conn():
+async def test_mongo_conn():
     try:
-        client.admin.command('ping')
+        await client.admin.command('ping')
         print(f"ping {MONGO_CLIENT}")
     except Exception as e:
         print(e)
