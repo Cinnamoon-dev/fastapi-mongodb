cb20e687e5737ebf8f8178f746064fe3aa6b6c8b
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-06 18:54:09 -0300

    feat: document update function created for edit service

---
+++ app/services/__init__.py
@@ -0,0 +1,15 @@
+def document_update(document: dict, request_json: dict) -> None:
+    document_keys = document.keys()
+
+    for key in document_keys:
+        if key in request_json and request_json[key] is not None:
+            try:
+                document[key] = request_json[key]
+            except:
+                setattr(document, key, request_json[key])
+
+    if request_json.get("email"):
+        try:
+            document["email"] = request_json.get("email").lower() # type: ignore
+        except:
+            setattr(document, "email", request_json.get("email").lower()) # type: ignore
