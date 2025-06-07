4b7b18fb885261d8b93955b967991fa46ddfbf2d
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-07 19:44:32 -0300

    feat: name check added to user type

--- app/models/userTypeModel.py
+++ app/models/userTypeModel.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 class UserType(BaseModel):
     name: str
+
+    @model_validator(mode="after")
+    def check_name(self):
+        if self.name == "":
+            raise ValueError("Name field should not be empty!")
+        return self
--- app/schemas/userTypeSchema.py
+++ app/schemas/userTypeSchema.py
@@ -1,8 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 
 class UserTypeAddSchema(BaseModel):
     name: str
 
+    @model_validator(mode="after")
+    def check_name(self):
+        if self.name == "":
+            raise ValueError("Name field should not be empty!")
+        return self
+
 class UserTypeEditSchema(BaseModel):
     name: str
+
+    @model_validator(mode="after")
+    def check_name(self):
+        if self.name == "":
+            raise ValueError("Name field should not be empty!")
+        return self
