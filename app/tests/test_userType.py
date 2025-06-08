50005242b238f5676f953b480e4ee91d95510930
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-08 19:53:42 -0300

    feat: edit invalid user type and edit repeated user type test case created

--- app/tests/test_userType.py
+++ app/tests/test_userType.py
@@ -4,10 +4,12 @@ from fastapi.testclient import TestClient
 from main import app
 
 # Casos de teste
+# ----------------------------------------------------
 # Criar um tipo de usuario valido
 # Criar um tipo de usuario com uma string vazia
 # Criar um tipo de usuario com um nome ja existente
 # ----------------------------------------------------
+# Editar um tipo de usuario sem mandar o corpo da requisição, ou com o corpo vazio
 # Editar um tipo de usuario para um nome valido
 # Editar um tipo de usuario para uma string vazia
 # Editar um tipo de usuario para um nome ja existente
@@ -15,6 +17,7 @@ from main import app
 # Deletar um tipo de usuario que nao esta sendo usado
 # Deletar um tipo de usuario que esta sendo usado
 # Deletar um tipo de usuario que nao existe
+# ----------------------------------------------------
 
 def create_user_type(name: str) -> str:
     """ Retorna uma string do dict criado, usar em `client.method(content=STRING)`. """
@@ -27,7 +30,7 @@ def create_user_type(name: str) -> str:
 @pytest.fixture(scope="session")
 def client():
     with TestClient(app) as test_client:
-            yield test_client
+        yield test_client
 
 def test_create_valid_user_type(client):
     content = create_user_type("valid_user_type_name")
@@ -49,3 +52,23 @@ def test_edit_valid_user_type(client):
     existing_user_type = client.get("/user/type/view/name/valid_user_type_name").json()
     response = client.put(f"/user/type/edit/{existing_user_type["data"]["id"]}", content=content)
     assert response.status_code == 200
+
+def test_edit_invalid_user_type(client):
+    type_to_edit = create_user_type("type_to_edit")
+    client.post("/user/type/add", content=type_to_edit)
+
+    content = create_user_type("")
+    existing_user_type = client.get("/user/type/view/name/type_to_edit").json()
+    response = client.put(f"/user/type/edit/{existing_user_type["data"]["id"]}", content=content)
+    assert response.status_code == 422
+
+def test_edit_repeated_user_type(client):
+    mocked_type = create_user_type("mocked_type")
+    existing_type = create_user_type("existing_type")
+
+    client.post("/user/type/add", content=mocked_type)
+    client.post("/user/type/add", content=existing_type)
+
+    mocked_type_with_id = client.get("/user/type/view/name/mocked_type").json()
+    response = client.put(f"/user/type/edit/{mocked_type_with_id["data"]["id"]}", content=existing_type)
+    assert response.status_code == 422
