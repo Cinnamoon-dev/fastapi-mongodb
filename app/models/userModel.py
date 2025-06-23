edfc6d5fa56f361d77801c01fd894a0589513d1a
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-23 18:23:50 -0300

    feat: name and password checks added to user model

--- app/models/userModel.py
+++ app/models/userModel.py
@@ -9,6 +9,18 @@ class User(BaseModel):
     user_type_id: str
 
     @model_validator(mode="after")
+    def check_name(self):
+        if self.name == "":
+            raise ValueError("Name field should not be empty!")
+        return self
+
+    @model_validator(mode="after")
+    def check_password(self):
+        if self.password == "":
+            raise ValueError("Password field should not be empty!")
+        return self
+
+    @model_validator(mode="after")
     def validate_user_type_id(self):
         if not ObjectId.is_valid(self.user_type_id):
             raise ValueError("Invalid user_type_id!")
