25eabfac17fefb624b7ec2d8cd675cf17bdfe094
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-27 20:56:42 -0300

    chore: generic Exception added to try-catch

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -5,11 +5,11 @@ def document_update(document: dict, request_json: dict) -> None:
         if key in request_json and request_json[key] is not None:
             try:
                 document[key] = request_json[key]
-            except:
+            except Exception:
                 setattr(document, key, request_json[key])
 
     if request_json.get("email"):
         try:
             document["email"] = request_json.get("email").lower()  # type: ignore
-        except:
+        except Exception:
             setattr(document, "email", request_json.get("email").lower())  # type: ignore
