7afd1aed3243c744b5e6a14f3a4b65d7a85aabb7
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-07-13 18:05:53 -0300

    build(ci): pulling new changes in script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+readonly git_path="/home/ubuntu/fastapi-mongodb"
+
 readonly docker_user="cinnamoon"
 readonly docker_image="fastapimongopipe"
 readonly docker_tag="latest"
 
+echo "PULLING NEW VERSION..."
+git -C ${git_path} pull &&
+echo "DONE"
+
 echo "STOPPING OLD VERSION..."
-docker compose -f compose.deploy.yaml down -t 1
+docker compose -f compose.deploy.yaml down -t 1 &&
 echo "DONE"
 
 echo "PULLING NEW IMAGE..."
-docker pull ${docker_user}/${docker_image}:${docker_tag}
+docker pull ${docker_user}/${docker_image}:${docker_tag} &&
 echo "DONE"
 
 echo "STARTING NEW VERSION..."
-docker compose -f compose.deploy.yaml up --build -d
+docker compose -f compose.deploy.yaml up --build -d &&
 echo "DONE"
